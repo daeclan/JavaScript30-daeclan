e7ce31cc53dd30d14638921e3f1d646b6b2a727b
Declan Murphy <declxn@gmail.com>
2020-10-15 19:32:10 -0500

    Fixed a discrepancy in the broken chain.

--- 08 - Fun with HTML5 Canvas/idk.rb
+++ 08 - Fun with HTML5 Canvas/idk.rb
@@ -12,5 +12,5 @@ puts "[#{answ},#{ered}]"
 # url = "https://translifeline.org/hiring_stats_sample/"
 # r = Net::HTTP.get_response(URI.parse(url).host, URI.parse(url).path)
 
-# p if you are seeing these changes, i am trying to account for the nights i worked on JS30 that didnt get committed or pushed due to my folder not being tracked. embarassing, but fixable. kinda.
+# p if you are seeing these changes, i am trying to account for the nights i worked on JS30 that didnt get committed or pushed due to my folder not being tracked. embarassing, but fixable. kinda
 
