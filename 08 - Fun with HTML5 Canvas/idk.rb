fb397e9ad1192524b033cfe7df913c424e213a96
Declan Murphy <declxn@gmail.com>
2020-11-02 11:35:07 -0600

    Committing JS30 progress, was tracking the wrong folder for the entirety of October.

---
+++ 08 - Fun with HTML5 Canvas/idk.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+file = open('https://translifeline.org/hiring_stats_sample')
+contents = file.read
+answ = contents[12, 3]
+ered = contents[15, 3]
+puts "[#{answ},#{ered}]"
+
+
+
+# require 'uri'
+# require 'net/http'
+# url = "https://translifeline.org/hiring_stats_sample/"
+# r = Net::HTTP.get_response(URI.parse(url).host, URI.parse(url).path)
+
+# p r
+
