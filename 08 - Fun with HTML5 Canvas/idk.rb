1d529b89a0db8fb8679cefac4f429fe4f37094e1
Declan Murphy <declxn@gmail.com>
2020-11-04 23:13:31 -0600

    Updated the canvas exercise, got the brush working.

--- 08 - Fun with HTML5 Canvas/idk.rb
+++ 08 - Fun with HTML5 Canvas/idk.rb
@@ -12,5 +12,4 @@ puts "[#{answ},#{ered}]"
 # url = "https://translifeline.org/hiring_stats_sample/"
 # r = Net::HTTP.get_response(URI.parse(url).host, URI.parse(url).path)
 
-# p if you are seeing these changes, i am trying to account for the nights i worked on Js30 but didn't commit. darkening, 10/15/2020.
 
